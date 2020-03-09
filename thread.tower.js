f3b63396da80aa99a4f635feaa9e5ad7b2113024
穆鸿翔 <mutou.leo.wood@gmail.com>
2020-03-09 21:34:36 +0800

    fix bug

--- thread.tower.js
+++ thread.tower.js
@@ -11,14 +11,14 @@ let Export = {
                         tower.attack(target);
                     }
                     if (towerName == "heal") {
-                        let target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+                        let target = tower.pos.findClosestByPath(FIND_MY_CREEPS, {
                             filter: object => object.hits < object.hitsMax
                         });
                         if (target == null) continue;
                         tower.heal(target);
                     }
                     if (towerName == "repair") {
-                        let target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                        let target = tower.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                             filter: object => object.hits < object.hitsMax
                         });
                         if (target == null) continue;
